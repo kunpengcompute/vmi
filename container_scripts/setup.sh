4471d3ecafabb4f9b3c241ce6c89feca3ae81372
linyufeng <yflin@bu.edu>
2023-06-08 15:41:22 +0800

    modify script

--- container_scripts/setup.sh
+++ container_scripts/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-# Script for setting up docker env in Kylin OS
+# Script for setting up docker env in Kylin v10 sp2/Euler 22.03 LTS SP1
 
 cur_file_path=$(pwd)
+
+IMAGE_NAME="ubuntu:2004"
+IMAGE_VERSION="ubuntu20.04"
+IMAGE_CUDA_VERSION="11.7.0"
+IMAGE_USER="$2"
 
 install_docker()
 {
@@ -16,7 +21,6 @@ install_docker()
     rm -rf ${DOCKER_INSTALL_LOC}/*
 
     # Download docker19.03 rpms since nvidia-docker required docker ver >= 19.03
-
     wget https://download.docker.com/linux/centos/8/aarch64/stable/Packages/containerd.io-1.6.16-3.1.el8.aarch64.rpm
     [ $? -ne 0 ] && echo "Download containerd.io failed" && exit
     wget https://download.docker.com/linux/centos/8/aarch64/stable/Packages/docker-ce-19.03.15-3.el8.aarch64.rpm
@@ -25,8 +29,6 @@ install_docker()
     [ $? -ne 0 ] && echo "Download docker-ce-cli failed" && exit
     wget https://download.docker.com/linux/centos/8/aarch64/stable/Packages/docker-compose-plugin-2.6.0-3.el8.aarch64.rpm
     [ $? -ne 0 ] && echo "Download docker-compose-plugin failed" && exit
-    #wget https://download.docker.com/linux/centos/8/aarch64/stable/Packages/docker-buildx-plugin-0.10.2-1.el8.aarch64.rpm
-    #[ $? -ne 0 ] && echo "Download docker-buildx-plugin failed" && exit
 
     echo "Installing docker rpms"
     sudo rpm -ivh *.rpm
@@ -49,61 +51,40 @@ install_docker()
 install_nvidia_container_toolkit()
 {
     echo "Installing nvidia container toolkit"
-    BASE_DIR=/root/nvidia-docker
+    BASE_DIR=/root/nvidia-container-toolkit
     if [ ! -d ${BASE_DIR} ]; then
         mkdir -p ${BASE_DIR}
     fi
-
-    LIBNVIDIA_CONTAINER_LOC=${BASE_DIR}/libnvidia-container
       
     cd ${BASE_DIR}
     rm -rf ${BASE_DIR}/*
-
-    # Download and compile libnvidia-container
-    git clone https://github.com/NVIDIA/libnvidia-container.git
-    [ $? -ne 0 ] && echo "clone libnvidia-container failed" && exit
-    cd libnvidia-container
-    local sum=0
-    make centos8
-    # ensure network is ok, 10 tries maximum
-    while [ $? -ne 0 ]
-    do
-        let sum++
-        [ ${sum} -eq 10 ] && echo "Reached maximum retry, make failed" && exit
-        echo "retrying make libnvidia-container for ${sum} times"
-        make centos8 && break
-    done
-
-    cd -
 
     # Download and compile container-toolkit
     CONTAINER_TOOLKIT_LOC=${BASE_DIR}/container-toolkit
 
     cd ${BASE_DIR}
     git clone https://gitlab.com/nvidia/container-toolkit/container-toolkit.git
+    if [ $? -ne 0 ]; then
+        echo "Git clone failed"
+        exit 1
+    fi
     cd container-toolkit
-    sum=0
-    make centos8
+    local sum=0
+    make centos8-aarch64
     while [ $? -ne 0 ]
     do
         let sum++
-        [ ${sum} -eq 10 ] && echo "Reached maximum retry, make failed" && exit
+        [ ${sum} -eq 10 ] && echo "Reached maximum retry, make failed" && exit 1
         echo "retrying make for container-toolkit ${sum} times"
-        make centos8 && break
+        make centos8-aarch64 && break
     done
     cd -
 
-    # install libnvidia-container
-    cd ${LIBNVIDIA_CONTAINER_LOC}/dist/centos8/aarch64
-    echo "Installing libnvidia-container"
-    sudo rpm -ivh *.rpm
-    [ $? -ne 0 ] && echo "install libnvidia-container failed" && exit
-
-    # install container-toolkit
+    # install rpms after success compilation
     cd ${CONTAINER_TOOLKIT_LOC}/dist/centos8/aarch64
     echo "Installing container-toolkit"
     sudo rpm -ivh *.rpm
-    [ $? -ne 0 ] && echo "install container-toolkit failed" && exit
+    [ $? -ne 0 ] && echo "install container-toolkit failed" && exit 1
 
     # register nvidia runtime
     echo "registering nvidia runtime"
@@ -119,17 +100,20 @@ build_ubuntu_image()
     
     echo "Building ubuntu image..."
     docker build --pull -t \
-       container:2004 \
-       --build-arg BASE_DIST=ubuntu20.04 \
-       --build-arg CUDA_VERSION=11.7.0 \
+       ${IMAGE_NAME} \
+       --build-arg BASE_DIST=${IMAGE_VERSION} \
+       --build-arg CUDA_VERSION=${IMAGE_CUDA_VERSION} \
        --file dockerfile .
     [ $? -eq 0 ] && echo "Build ubuntu image success" && cd - && return 0
-    echo "Build ubuntu image failed" && exit
+    echo "Build ubuntu image failed" && exit 1
 }
 
 create_container()
 {
     echo "Running container..."
+    if [ ! -z "$IMAGE_USER"]; then
+        IMAGE_NAME=$IMAGE_USER
+    fi
     CONTAINER_ID=$(docker run --gpus all --privileged \
         -e NVIDIA_DISABLE_REQUIRE=1 \
 	-v $HOME/.Xauthority:/root/.Xauthority \
@@ -137,28 +121,28 @@ create_container()
 	--device /dev/dri --net host \
 	-v /etc/vulkan/icd.d/nvidia_icd.json:/etc/vulkan/icd.d/nvidia_icd.json \
 	-v /etc/vulkan/implicit_layer.d/nvidia_layers.json:/etc/vulkan/implicit_layer.d/nvidia_layers.json \
-	-v /usr/share/glvnd/egl_vendor.d/10_nvidia.json:/usr/share/glvnd/egl_vendor.d/10_nvidia.json -itd container:2004)
+	-v /usr/share/glvnd/egl_vendor.d/10_nvidia.json:/usr/share/glvnd/egl_vendor.d/10_nvidia.json -itd ${IMAGE_NAME})
     [ $? -eq 0 ] && echo "Create container success" && return 0
-    echo "Create container failed" && exit
+    echo "Create container failed" && exit 1
 }
 
 default_set_up()
 {
     install_docker
-    [ $? -ne 0 ] && echo "install docker 19.03 failed" && exit
+    [ $? -ne 0 ] && echo "install docker 19.03 failed" && exit 1
     install_nvidia_container_toolkit
-    [ $? -ne 0 ] && echo "install nv docker failed" && exit
+    [ $? -ne 0 ] && echo "install nv docker failed" && exit 1
     build_ubuntu_image
-    [ $? -ne 0 ] && echo "build ubuntu image failed" && exit
+    [ $? -ne 0 ] && echo "build ubuntu image failed" && exit 1
     create_container
-    [ $? -ne 0 ] && echo "create container failed" && exit
+    [ $? -ne 0 ] && echo "create container failed" && exit 1
     return 0
 }
 
 setup_new_container()
 {
     create_container
-    [ $? -ne 0 ] && echo "create container failed" && exit
+    [ $? -ne 0 ] && echo "create container failed" && exit 1
     echo "Installing dependency for container ${CONTAINER_ID}"
     docker exec -it ${CONTAINER_ID} /bin/bash -c 'export DEBIAN_FRONTEND=noninteractive'
     docker exec -it ${CONTAINER_ID} /bin/bash -c 'sudo apt-get update'
@@ -175,7 +159,7 @@ setup_new_container_with_exagear()
     set_up_new_container
     cd cur_file_path
     if [ ! -f "ExaGear*.tar.gz" ]; then
-        echo "ExaGear package not exists" && exit
+        echo "ExaGear package not exists" && exit 1
     fi
     docker cp ExaGear*.tar.gz ${CONTAINER_ID}:/root
     if [ $? -ne 0 ]; then
@@ -205,17 +189,16 @@ clean_up()
 ACTION=$1; shift
 echo "Preferred action is: ${ACTION}"
 case "$ACTION" in
-    default) default_set_up "@"
+    default) default_set_up
     ;;
-    new) setup_new_container "@"
+    new) setup_new_container
     ;;
-    exagear) setup_new_container_with_exagear "@"
+    exagear) setup_new_container_with_exagear
     ;;
-    build ) build_ubuntu_image "@"
+    build) build_ubuntu_image
     ;;
-    clean) clean_up "@"
+    clean) clean_up
     ;;
     *) echo "Incorrect input, ${ACTION} is not supported"
     ;;
 esac
-    
