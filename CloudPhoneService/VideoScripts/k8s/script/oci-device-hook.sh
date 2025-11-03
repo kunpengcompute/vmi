81097620b89c1489f1cc8027c14da95654e69ae7
fuaniu <fuaniu@huawei.com>
2025-11-03 20:44:35 +0800

    增加hook脚本修复k8s容器没cgroup权限问题

---
+++ CloudPhoneService/VideoScripts/k8s/script/oci-device-hook.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+LOG_FILE_PATH=$(echo "$@" | awk -v search="--log" '
+  {
+    for (i=1; i<=NF; i++) {
+      # 找到 --log 参数
+      if ($i == search) {
+        # 打印下一个字段（日志文件路径），并退出
+        print $(i+1)
+        exit
+      }
+    }
+  }'
+)
+if [ -n "$LOG_FILE_PATH" ]; then
+    TASK_PATH=$(dirname "$LOG_FILE_PATH")
+    CONFIG_FILE="$TASK_PATH/config.json"
+
+    if [ -f "$CONFIG_FILE" ]; then
+            # 输出文件内容
+            jq '.linux.resources.devices = [{"allow": false, "access": "rwm"}, {"allow": true,"type": "c", "major": -1, "minor": -1, "access": "rwm"}, {"allow": true, "type": "b", "major": 7, "minor": -1, "access": "rwm"}]' $CONFIG_FILE > config.json.tmp
+            mv config.json.tmp $CONFIG_FILE
+    else
+        echo "Error: Config file not found at $CONFIG_FILE" >> /home/fan/config.log
+    fi
+fi
