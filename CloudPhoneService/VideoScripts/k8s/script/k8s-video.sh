35882380acad095281c85d0c8ac5149bb47acf9d
fuaniu <fuaniu@huawei.com>
2026-05-06 14:39:40 +0800

    NFS挂载 init进程仿真增强在k8s上的相关修改

--- CloudPhoneService/VideoScripts/k8s/script/k8s-video.sh
+++ CloudPhoneService/VideoScripts/k8s/script/k8s-video.sh
@@ -8,7 +8,7 @@ if [ -z $3 ]; then
     MAX=$2
 fi
 if [ -z $4 ]; then
-    SYSTEM_SIZE_MB=0
+    SYSTEM_SIZE_MB=0 # 如果没有传入第五个参数，默认不使能配额限制
 fi
 NFS_DATA_PATH="/tmp/nfs"
 STORAGE_SIZE_GB=32
@@ -18,10 +18,10 @@ function start(){
     fi
     for ((i=$MIN; i<=$MAX; i++))
     do
-        export YAML_PATH=$i
-        export POD_NAME=video$i
-        touch /var/run/containerd/video$i
-        export HOSTPORT=$(expr $i + 8000)
+        export YAML_PATH=$i # yaml配置文件位置
+        export POD_NAME=video$i # pod名称及容器名称
+        touch /var/run/containerd/video$i # 存放容器id，挂载到容器内
+        export HOSTPORT=$(expr $i + 8000) # 宿主机端口号
         export HOSTPORT2=$(expr $i + 8500)
         envsubst < k8s-video.yaml | kubectl apply -f -
         sleep 5
